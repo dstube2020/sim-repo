921cedcf4183e68df1096dd38380741578287b3a
simonabreha93@gmail.com <simonabreha93@gmail.com>
2019-08-29 09:58:50 +0300

    palindrome

--- homework.js
+++ homework.js
@@ -17,3 +17,22 @@ function reverseNumber(){
   
  window.alert(`the reverse number is : ${reverse}`);
 }
+
+
+function palindrome(){
+    //   const x = prompt('enter a word for checkup');
+  const x = window.prompt("enter a number for checkup"); 
+  let palindrome = '';
+ for(let i = x.length-1 ;i >= 0 ; i--){
+      let y = x.charAt(i);
+      palindrome = palindrome.concat(y) ;
+   } 
+   if (x == palindrome){
+       window.alert("palindrome"); 
+   }
+   else{
+       window.alert('not palindrome')
+   }
+   
+
+}
